4c801376b1b1a209a7e4072ce72f46c58f96df01
filip doganowski <filip.doganowski@gmail.com>
2023-01-12 21:33:53 +0100

    Add BlocProvider & BlocBuilder

--- lib/app/features/root/root_page.dart
+++ lib/app/features/root/root_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:travel_cost_log/app/cubit/root_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({
@@ -7,6 +9,21 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return BlocProvider(
+      create: (context) => RootCubit()..start(),
+      child: BlocBuilder<RootCubit, RootState>(
+        builder: (context, state) {
+          final user = state.user;
+
+          if (state.isLoading) {
+            return const Scaffold();
+          }
+          if (user == null) {
+            return const Scaffold();
+          }
+          return const Scaffold();
+        },
+      ),
+    );
   }
 }
