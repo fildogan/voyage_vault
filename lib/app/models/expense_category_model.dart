397c8043403ce8172bffa20c429a52f5b2f87216
filip doganowski <filip.doganowski@gmail.com>
2023-01-15 15:31:02 +0100

    Add ExpenseModel, ExpenseCategoryModel and ExpenseCategoryRepository

---
+++ lib/app/models/expense_category_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpenseCategoryModel {
+  ExpenseCategoryModel(
+    this.categoryTitle,
+    this.categoryIcon,
+  );
+  final String categoryTitle;
+  final Icon categoryIcon;
+}
--- lib/app/models/expense_model.dart
+++ lib/app/models/expense_model.dart
@@ -1,11 +1,15 @@
+import 'package:travel_cost_log/app/models/expense_category_model.dart';
+
 class ExpenseModel {
   ExpenseModel(
     this.expenseID,
     this.title,
     this.travelID,
+    this.expenseCategory,
   );
 
   final String expenseID;
   final String travelID;
   final String title;
+  final ExpenseCategoryModel expenseCategory;
 }
