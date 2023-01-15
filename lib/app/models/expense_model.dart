258d2bc3bc960cbd4d9e1946952f1e26265b2fed
filip doganowski <filip.doganowski@gmail.com>
2023-01-15 13:22:51 +0100

    Create expense model

---
+++ lib/app/models/expense_model.dart
@@ -0,0 +1,11 @@
+class ExpenseModel {
+  ExpenseModel(
+    this.expenseID,
+    this.title,
+    this.travelID,
+  );
+
+  final String expenseID;
+  final String travelID;
+  final String title;
+}
