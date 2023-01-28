3668680cebc8eaf78579dc87fe33562569b32514
filip doganowski <filip.doganowski@gmail.com>
2023-01-28 12:58:13 +0100

    Update AccountPage UI

--- lib/features/home/pages/my_account/my_account_page.dart
+++ lib/features/home/pages/my_account/my_account_page.dart
@@ -8,19 +8,61 @@ class MyAccountPageContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.person),
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const UserProfile(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: ListView(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.person),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const UserProfile(),
+                  ),
                 ),
+                child: const Text('Go to user page'),
               ),
-              child: const Text('Go to user page'),
-            )
-          ],
+              const Text('Settings'),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black26),
+                child: Column(
+                  children: [
+                    ListTile(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const UserProfile(),
+                          ),
+                        );
+                      },
+                      title: const Text('Account settings'),
+                      trailing: const Icon(Icons.chevron_right),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black38),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const ListTile(
+                      title: Text('1'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
