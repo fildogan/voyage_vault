d689b3ab38bd1ba3e47bd02aa39c25bc02259e07
filip doganowski <filip.doganowski@gmail.com>
2023-02-15 23:05:15 +0100

    Create language arb files

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:voyage_vault/app/core/config.dart';
 import 'package:voyage_vault/app/cubit/root_cubit.dart';
 import 'package:voyage_vault/features/auth/pages/auth_gate.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,10 +16,22 @@ class MyApp extends StatelessWidget {
           builder: (context, state) {
             return MaterialApp(
               debugShowCheckedModeBanner: Config.debugShowCheckedModeBanner,
-              title: 'Travel Cost Log',
+              title: 'Voyage Vault',
               theme: ThemeData.light(useMaterial3: true),
               darkTheme: ThemeData.dark(useMaterial3: true),
               themeMode: state.currentTheme,
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: const [
+                Locale('en'), // English
+                Locale('pl'), // Polish
+                Locale('fr'), // French
+                Locale('it'), // Italian
+                Locale('es'), // Spanish
+              ],
               home: const AuthGate(),
             );
           },
