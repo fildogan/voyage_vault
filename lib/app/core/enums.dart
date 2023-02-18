0a017898f6ebef1138e2b0a1bc4f394d3164504e
filip doganowski <filip.doganowski@gmail.com>
2023-02-18 18:57:49 +0100

    Add SelectedTheme enum

--- lib/app/core/enums.dart
+++ lib/app/core/enums.dart
@@ -3,3 +3,5 @@ enum Status { initial, loading, success, error }
 enum FormStatus { initial, submitting, success, error }
 
 enum SelectedLanguage { english, spanish, french, italian, polish, system }
+
+enum SelectedTheme { dark, light, system }
