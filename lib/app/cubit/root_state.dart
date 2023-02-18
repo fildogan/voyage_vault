2ad445d9e46a2989e9f0874c96f77669f88d1273
filip doganowski <filip.doganowski@gmail.com>
2023-02-18 19:04:01 +0100

    Add getters to root state

--- lib/app/cubit/root_state.dart
+++ lib/app/cubit/root_state.dart
@@ -5,9 +5,36 @@ class RootState with _$RootState {
   factory RootState({
     @Default(Status.initial) Status status,
     @Default(SelectedLanguage.system) SelectedLanguage selectedLanguage,
-    @Default(ThemeMode.system) ThemeMode currentTheme,
-    Locale? locale,
+    @Default(SelectedTheme.system) SelectedTheme selectedTheme,
     PackageInfo? packageInfo,
   }) = _RootState;
   const RootState._();
+
+  Locale? get locale {
+    switch (selectedLanguage) {
+      case SelectedLanguage.english:
+        return const Locale('en');
+      case SelectedLanguage.spanish:
+        return const Locale('es');
+      case SelectedLanguage.french:
+        return const Locale('fr');
+      case SelectedLanguage.italian:
+        return const Locale('it');
+      case SelectedLanguage.polish:
+        return const Locale('pl');
+      case SelectedLanguage.system:
+        return null;
+    }
+  }
+
+  ThemeMode get currentTheme {
+    switch (selectedTheme) {
+      case SelectedTheme.light:
+        return ThemeMode.light;
+      case SelectedTheme.dark:
+        return ThemeMode.dark;
+      case SelectedTheme.system:
+        return ThemeMode.system;
+    }
+  }
 }
