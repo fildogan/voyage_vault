b351b06f779205065444f9fc052380dfaa565f42
filip doganowski <filip.doganowski@gmail.com>
2023-02-19 11:43:32 +0100

    Implement freezed and jsonserializable for quote model

--- lib/domain/models/quote_model.dart
+++ lib/domain/models/quote_model.dart
@@ -1,36 +1,34 @@
-// import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-// part 'quote_model.freezed.dart';
+part 'quote_model.freezed.dart';
 
-// part 'quote_model.g.dart';
+part 'quote_model.g.dart';
 
-// RUN IN TERMINAL: flutter packages pub run build_runner build
-// OR: flutter pub run build_runner build --delete-conflicting-outputs
+@freezed
+class QuoteModel with _$QuoteModel {
+  factory QuoteModel({
+    required int id,
+    required String title,
+    required String author,
+  }) = _QuoteModel;
 
-// @freezed
-// class QuoteModel with _$QuoteModel {
-//   factory QuoteModel(
-//     int id,
-//     String title,
-//     String author,
-//   ) = _QuoteModel;
-//   factory QuoteModel.fromJson(Map<String, dynamic> json) =>
-//       _$QuoteModelFromJson(json);
-// }
-
-class QuoteModel {
-  const QuoteModel({
-    required this.id,
-    required this.title,
-    required this.author,
-  });
-
-  final int id;
-  final String title;
-  final String author;
-
-  QuoteModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        title = json['title'],
-        author = json['author'];
+  factory QuoteModel.fromJson(Map<String, dynamic> json) =>
+      _$QuoteModelFromJson(json);
 }
+
+// class QuoteModel {
+//   const QuoteModel({
+//     required this.id,
+//     required this.title,
+//     required this.author,
+//   });
+
+//   final int id;
+//   final String title;
+//   final String author;
+
+//   QuoteModel.fromJson(Map<String, dynamic> json)
+//       : id = json['id'],
+//         title = json['title'],
+//         author = json['author'];
+// }
