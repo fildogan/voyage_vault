3cd29b2b317ee3a7b6533345a9260bf4976f7b37
filip doganowski <filip.doganowski@gmail.com>
2023-02-19 11:40:40 +0100

    Implement retrofit to quotes data source

--- lib/data/data_sources/remote_data_sources/quotes_remote_data_source.dart
+++ lib/data/data_sources/remote_data_sources/quotes_remote_data_source.dart
@@ -1,15 +1,29 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:voyage_vault/domain/models/quote_model.dart';
+
+part 'quotes_remote_data_source.g.dart';
 
 @injectable
-class QuotesRemoteDioDataSource {
-  Future<List<Map<String, dynamic>>?> getQuotes() async {
-    final response = await Dio().get<List<dynamic>>(
-        'https://my-json-server.typicode.com/fildogan/json-demo/quotes');
-    final listDynamic = response.data;
-    if (listDynamic == null) {
-      return null;
-    }
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-  }
+@RestApi()
+abstract class QuotesRemoteRetroFitDataSource {
+  @factoryMethod
+  factory QuotesRemoteRetroFitDataSource(Dio dio) =
+      _QuotesRemoteRetroFitDataSource;
+
+  @GET('/quotes')
+  Future<List<QuoteModel>> getQuotes();
 }
+
+// class QuotesRemoteDioDataSource {
+//   Future<List<Map<String, dynamic>>?> getQuotes() async {
+//     final response = await Dio().get<List<dynamic>>(
+//         'https://my-json-server.typicode.com/fildogan/json-demo/quotes');
+//     final listDynamic = response.data;
+//     if (listDynamic == null) {
+//       return null;
+//     }
+//     return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+//   }
+// }
