c71716a9820e94011181c175761b8f165880ff3f
filip doganowski <filip.doganowski@gmail.com>
2023-02-27 21:01:14 +0100

    Add initials getter for voyager model

--- lib/domain/models/voyager_model.dart
+++ lib/domain/models/voyager_model.dart
@@ -19,4 +19,18 @@ class VoyagerModel with _$VoyagerModel {
   VoyagerModel setSelected(bool value) {
     return copyWith(isSelected: value);
   }
+
+  // Define a getter method to get initials
+  String get initials {
+    final words = name.split(' ');
+    if (words.isEmpty) {
+      return '';
+    }
+    final firstInitial = words[0].substring(0, 1).toUpperCase();
+    if (words.length == 1) {
+      return firstInitial;
+    }
+    final secondInitial = words[1].substring(0, 1).toUpperCase();
+    return '$firstInitial$secondInitial';
+  }
 }
